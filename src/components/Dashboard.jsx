547fcca767f16ecf670c865732de77bfef8eb0b0
admmintimser <adminti@timsergroup.com>
2024-05-22 10:23:53 -0600

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,209 +1,211 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Context } from "../main";
-import { Navigate } from "react-router-dom";
-import axios from "axios";
-import { toast } from "react-toastify";
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { questions } from './questionData';
+import SliderInput from './SliderInput';
+import './AppointmentForm.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const locationMapping = {
-    '16 de Septiembre': 1915,
-    '16 de septiembre': 1915,
-    'Suprema Corte': 1916,
-    'Bolivar': 1917,
-    'Pino Suaréz': 1918,
-    'Toluca': 1919,
-    'Chimalpopoca': 1920
-};
+const AppointmentForm = () => {
+    const { register, handleSubmit, setValue, getValues, reset, formState: { errors } } = useForm();
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [isFormComplete, setIsFormComplete] = useState(false);
+    const [showFastingError, setShowFastingError] = useState(false);
+    const [weight, setWeight] = useState(70); // Default weight
+    const [height, setHeight] = useState(170); // Default height
+    const [submittedData, setSubmittedData] = useState(null);
 
-const Dashboard = () => {
-    const [appointments, setAppointments] = useState([]);
-    const [tokenDevel, setTokenDevel] = useState(null);
-    const { isAuthenticated, authToken, admin } = useContext(Context);
-    const [searchTerm, setSearchTerm] = useState('');
+    const onSubmit = async (formData) => {
+        const calculateFastingHours = (lastMealTimeStr) => {
+            if (!lastMealTimeStr) 
+                return 0;
+            
+            const lastMealDate = new Date();
+            const timeParts = lastMealTimeStr.split(':');
+            lastMealDate.setHours(timeParts[0], timeParts[1], 0, 0);
+
+            const currentDate = new Date();
+            const fastingMilliseconds = currentDate - lastMealDate;
+            const fastingHours = Math.floor(fastingMilliseconds / 3600000);
+
+            return fastingHours > 0
+                ? fastingHours
+                : 0;
+        };
+
+        const selectedLocation = formData.sampleLocation;
+        const sampleLocationQuestion = questions.find(q => q.field === 'sampleLocation');
+        const sampleLocationValue = sampleLocationQuestion.mapping[selectedLocation] || null;
+
+        const formattedData = {
+            ...formData,
+            privacyConsent: formData.privacyConsent === "Sí acepto",
+            informedConsent: formData.informedConsent === "Sí acepto",
+            weight: parseFloat(formData.weight) || 0,
+            height: parseFloat(formData.height) || 0,
+            lastMealTime: formData.lastMealTime
+                ? new Date(`2021-01-01T${formData.lastMealTime}:00`).toISOString()
+                : null,
+            fastingHours: calculateFastingHours(formData.lastMealTime),
+            sampleLocation: formData.sampleLocation || "default location",
+            sampleLocationValue: sampleLocationValue
+        };
+
+        console.log('Sending data:', formattedData);
+
+        try {
+            const response = await axios.post(
+                'https://webapitimser.azurewebsites.net/api/v1/appointment/post',
+                formattedData,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+            console.log('Response:', response.data);
+            toast.success('Formulario enviado con éxito');
+            setSubmittedData(formattedData);
+            setIsFormComplete(true);
+        } catch (error) {
+            console.error(
+                'Error al enviar formulario',
+                error.response?.data.message || error.message
+            );
+            toast.error(error.response?.data.message || "An error occurred while submitting the form.");
+        }
+    };
+
+    const handleNext = () => {
+        const currentQuestion = questions[currentQuestionIndex];
+        const answer = getValues()[currentQuestion.field];
+
+        if (currentQuestion.id === 'fastingHoursQ' && answer === 'No') {
+            setShowFastingError(true);
+        } else {
+            setShowFastingError(false);
+            if (currentQuestion.nextq) {
+                if (typeof currentQuestion.nextq === 'object') {
+                    const nextQuestionId = currentQuestion.nextq[answer];
+                    if (nextQuestionId === 'showFastingError') {
+                        setShowFastingError(true);
+                    } else if (nextQuestionId === 'close') {
+                        setIsFormComplete(true);
+                        handleSubmit(onSubmit)();
+                    } else {
+                        setCurrentQuestionIndex(questions.findIndex(q => q.id === nextQuestionId));
+                    }
+                } else if (currentQuestion.nextq === 'close') {
+                    setIsFormComplete(true);
+                    handleSubmit(onSubmit)();
+                } else {
+                    setCurrentQuestionIndex(questions.findIndex(q => q.id === currentQuestion.nextq));
+                }
+            } else {
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
+            }
+        }
+        if (currentQuestion && currentQuestion.type !== 'choice') {
+            setValue(currentQuestion.field, '');
+        }
+    };
+
+    const handleBack = () => {
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+        }
+    };
+
+    const handleAddAnotherForm = () => {
+        reset();
+        setCurrentQuestionIndex(0);
+        setIsFormComplete(false);
+        setShowFastingError(false);
+    };
+
+    const currentQuestion = questions[currentQuestionIndex];
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const { data } = await axios.get(
-                    "https://webapitimser.azurewebsites.net/api/v1/appointment/getall",
-                    { withCredentials: true }
-                );
-                setAppointments(data.appointments);
-            } catch (error) {
-                console.error("Error fetching data", error);
-                setAppointments([]);
-            }
-        };
-        fetchData();
-    }, []);
-
-    const handleUpdateDevelab = async (appointmentId, newStatus, appointment) => {
-        try {
-            // Verificar y asignar sampleLocationValue si está vacío
-            if (!appointment.sampleLocationValue && appointment.sampleLocation) {
-                appointment.sampleLocationValue = locationMapping[appointment.sampleLocation] || appointment.sampleLocationValue;
-            }
-
-            const currentDateTime = new Date().toISOString();
-            const { data } = await axios.put(
-                `https://webapitimser.azurewebsites.net/api/v1/appointment/update/${appointmentId}`,
-                {
-                    tomaEntregada: newStatus,
-                    tomaProcesada: true,
-                    fechaToma: currentDateTime,
-                },
-                { withCredentials: true }
-            );
-
-            setAppointments((prevAppointments) => prevAppointments.map(
-                (appt) => appt._id === appointmentId
-                    ? { ...appt, tomaEntregada: newStatus, tomaProcesada: true, fechaToma: currentDateTime }
-                    : appt
-            ));
-
-            toast.success("Estatus Develab actualizado con éxito");
-
-            if (newStatus) {
-                await performExternalApiCalls(appointment);
-            }
-        } catch (error) {
-            toast.error(
-                error.response?.data?.message || "Error al actualizar el estatus Develab"
-            );
+        if (currentQuestion && currentQuestion.type !== 'choice') {
+            setValue(currentQuestion.field, '');
         }
-    };
-
-    const performExternalApiCalls = async (appointment) => {
-        try {
-            const loginResponse = await axios.post("https://webapi.devellab.mx/api/Account/login", {
-                username: "mhs",
-                password: "cd098f3b9eae4ae7af3911aec1847d76"
-            });
-            const token = loginResponse.data.accessToken;
-            setTokenDevel(token);
-
-            const patientData = {
-                code: "",
-                name: appointment.patientFirstName,
-                lastname: appointment.patientLastName,
-                address: "",
-                phone: appointment.mobilePhone,
-                birthDate: appointment.birthDate,
-                gender: "F",
-                email: appointment.email,
-                comment: "",
-                customerId: -1,
-                extraField1: "",
-                extraField2: "",
-                extraField3: "",
-                extraField4: "",
-                extraField5: "",
-                extraField6: ""
-            };
-
-            const patientResponse = await axios.post("https://webapi.devellab.mx/api/Patient/", patientData, {
-                headers: {
-                    accept: "*/*",
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            const customerId = patientResponse.data.customerId;
-            appointment.ClienteDevelab = customerId;
-
-            const sampleDate = new Date(appointment.fechaToma).toISOString().slice(0, 16);
-
-            const orderData = {
-                branchId: 1,
-                patientId: customerId,
-                observations: "",
-                customerId: appointment.sampleLocationValue,
-                customerOrderNumber: "",
-                extraField1: "",
-                extraField2: "",
-                extraField3: "",
-                extraField4: "",
-                extraField5: "",
-                extraField6: "",
-                exams: [
-                    {
-                        examId: "E-470"
-                    }
-                ],
-                sampleDate: sampleDate
-            };
-
-            const orderResponse = await axios.post("https://webapi.devellab.mx/api/Order/", orderData, {
-                headers: {
-                    accept: "*/*",
-                    Authorization: `Bearer ${token}`
-                }
-            });
-
-            appointment.FolioDevelab = orderResponse.data.orderNumber;
-            appointment.OrderIDDevelab = orderResponse.data.orderId;
-            toast.success("Data sent to Develab successfully");
-        } catch (error) {
-            toast.error("Error while performing external API calls");
-            console.error(error);
-        }
-    };
-
-    if (!isAuthenticated) {
-      return <Navigate to="/login" />;
-    }
+    }, [currentQuestionIndex, setValue, currentQuestion]);
 
     return (
-        <section className="dashboard page">
-            <div className="banner">
-                <div className="firstBox">
-                    <div className="content">
-                        <div>
-                            <h5>{admin && `${admin.firstName} ${admin.lastName}`}</h5>
+        <div className="appointment-page">
+            <ToastContainer />
+            <div className="logos">
+                <img src="/Logo-Preventix.png" alt="logo" className="logo-img"/>
+                <img src="/BanorteMHS.png" alt="logoBanorte" className="logo-img"/>
+            </div>
+            <div className="container">
+                <div className="center">
+                    {showFastingError && (
+                        <div className="banner-error">
+                            Gracias por tu interés en aplicarte la prueba, pero no podemos realizártela si no cuentas con un ayuno menor a 6 horas.
                         </div>
-                    </div>
-                </div>
-                <div className="secondBox">
-                    <p>Pacientes</p>
-                    <h3>{appointments.length}</h3>
-                </div>
-                <div className="thirdBox">
-                    <p>Procesadas</p>
-                    <h3>{appointments.filter(appt => appt.tomaProcesada).length}</h3>
+                    )}
+                    {!isFormComplete ? (
+                        <form id="dynamic" onSubmit={handleSubmit(onSubmit)}>
+                            {currentQuestion.type === 'slider' ? (
+                                <SliderInput
+                                    label={currentQuestion.label}
+                                    min={currentQuestion.min}
+                                    max={currentQuestion.max}
+                                    value={currentQuestion.field === 'weight' ? weight : height}
+                                    onChange={(e) => currentQuestion.field === 'weight' ? setWeight(e.target.value) : setHeight(e.target.value)}
+                                    unit={currentQuestion.unit}
+                                />
+                            ) : (
+                                <div className="question-wrap">
+                                    <div className="question">{currentQuestion.question}</div>
+                                    <div className="answer">
+                                        {currentQuestion.type === 'choice' ? (
+                                            currentQuestion.answers.map((answer, index) => (
+                                                <div key={index}>
+                                                    <label>
+                                                        <input type="radio" value={answer} {...register(currentQuestion.field)} defaultChecked={index === 0} /> {answer}
+                                                    </label>
+                                                </div>
+                                            ))
+                                        ) : (
+                                            currentQuestion.type === 'time' && currentQuestion.id === 'lastMealTimeQ' ? (
+                                                <input
+                                                    type="time"
+                                                    {...register(currentQuestion.field)}
+                                                    min={new Date(new Date().getTime() - 4 * 60 * 60 * 1000).toISOString().substring(11, 16)}
+                                                />
+                                            ) : (
+                                                <input type={currentQuestion.type} {...register(currentQuestion.field)} />
+                                            )
+                                        )}
+                                    </div>
+                                </div>
+                            )}
+                            {currentQuestionIndex < questions.length - 1 && !showFastingError && (
+                                <button type="button" onClick={handleNext}>Siguiente</button>
+                            )}
+                            {currentQuestionIndex > 0 && (
+                                <button type="button" onClick={handleBack}>Atrás</button>
+                            )}
+                            
+                        </form>
+                    ) : (
+                        <div>
+                            <h2>¡Formulario enviado con éxito!</h2>
+                            <button onClick={handleAddAnotherForm} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Agregar otro Formulario
+                            </button>
+                            <button onClick={() => alert(JSON.stringify(submittedData, null, 2))} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                Revisar tus respuestas
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
-            <div className="banner">
-                <h5>Preventix</h5>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Lugar de toma</th>
-                            <th>Ayuno</th>
-                            <th>Tomada</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {appointments.length > 0 ? appointments.map((appointment) => (
-                            <tr key={appointment._id}>
-                                <td>{`${appointment.patientFirstName} ${appointment.patientLastName}`}</td>
-                                <td>{appointment.sampleLocation}</td>
-                                <td>{`${appointment.fastingHours} horas`}</td>
-                                <td>
-                                    <button
-                                        onClick={() => handleUpdateDevelab(appointment._id, true, appointment)}
-                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                    >
-                                        Procesar Toma
-                                    </button>
-                                </td>
-                            </tr>
-                        )) : <tr>
-                            <td colSpan="6">No appointments found.</td>
-                        </tr>}
-                    </tbody>
-                </table>
-            </div>
-        </section>
+        </div>
     );
 };
 
-export default Dashboard;
+export default AppointmentForm;
