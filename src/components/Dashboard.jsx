3ebfa602c3e931c7e24613617c1fb173cad0f419
admmintimser <adminti@timsergroup.com>
2024-05-31 00:14:38 -0600

    V2.0

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -24,16 +24,20 @@ const Dashboard = () => {
     const [editingAppointment, setEditingAppointment] = useState(null);
     const [formValues, setFormValues] = useState({});
     const [searchTerm, setSearchTerm] = useState(''); // Nueva variable de estado para el término de búsqueda
+    const [successfulUpdates, setSuccessfulUpdates] = useState({});
 
     const fetchData = async () => {
         try {
-            const { data } = await axios.get(
-                "https://webapitimser.azurewebsites.net/api/v1/appointment/getall",
-                { withCredentials: true }
-            );
-            setAppointments(data.appointments.reverse()); // Mostrar citas en orden inverso
+            const response = await axios.get("https://webapitimser.azurewebsites.net/api/v1/appointment/getall", { withCredentials: true });
+            console.log('Data fetched:', response.data.appointments); // Debugging
+            if (response.data.appointments) {
+                setAppointments(response.data.appointments.reverse());
+            } else {
+                throw new Error('No appointments data received');
+            }
         } catch (error) {
             console.error("Error fetching data", error);
+            toast.error("Error fetching appointments: " + error.message);
             setAppointments([]);
         }
     };
@@ -45,49 +49,62 @@ const Dashboard = () => {
     }, []);
 
     const handleUpdateDevelab = async (appointmentId, newStatus, appointment) => {
-        try {
-            if (!appointment.sampleLocationValue && appointment.sampleLocation) {
-                appointment.sampleLocationValue = locationMapping[appointment.sampleLocation] || appointment.sampleLocationValue;
-            }
-
-            const currentDateTime = new Date().toISOString();
-            const { data } = await axios.put(
-                `https://webapitimser.azurewebsites.net/api/v1/appointment/update/${appointmentId}`,
-                {
-                    tomaEntregada: newStatus,
-                    tomaProcesada: true,
-                    fechaToma: currentDateTime,
-                },
-                { withCredentials: true, headers: { Authorization: `Bearer ${authToken}` } }
-            );
-
-            setAppointments((prevAppointments) => prevAppointments.map(
-                (appt) => appt._id === appointmentId
-                    ? { ...appt, tomaEntregada: newStatus, tomaProcesada: true, fechaToma: currentDateTime }
-                    : appt
-            ));
-
-            toast.success("Estatus Develab actualizado con éxito");
-
-            if (newStatus) {
-                await performExternalApiCalls(appointment);
-            }
-        } catch (error) {
-            toast.error(
-                error.response?.data?.message || "Error al actualizar el estatus Develab"
-            );
+    // Verificar si ya fue procesada con éxito y pedir confirmación
+    if (successfulUpdates[appointmentId]) {
+        const confirm = window.confirm("Esta cita ya fue procesada con éxito. ¿Deseas enviar de nuevo?");
+        if (!confirm) {
+            return; // Detener si el usuario no confirma
         }
-    };
+    }
+
+    try {
+        if (!appointment.sampleLocationValue && appointment.sampleLocation) {
+            appointment.sampleLocationValue = locationMapping[appointment.sampleLocation] || appointment.sampleLocationValue;
+        }
+
+        const currentDateTime = new Date().toISOString();
+        const { data } = await axios.put(
+            `https://webapitimser.azurewebsites.net/api/v1/appointment/update/${appointmentId}`,
+            {
+                tomaEntregada: newStatus,
+                tomaProcesada: true,
+                fechaToma: currentDateTime,
+            },
+            { withCredentials: true, headers: { Authorization: `Bearer ${authToken}` } }
+        );
+
+        setAppointments((prevAppointments) => prevAppointments.map(
+            (appt) => appt._id === appointmentId
+                ? { ...appt, tomaEntregada: newStatus, tomaProcesada: true, fechaToma: currentDateTime }
+                : appt
+        ));
+
+        // Marcar como exitoso
+        setSuccessfulUpdates(prev => ({ ...prev, [appointmentId]: true }));
+        toast.success("Estatus Develab actualizado con éxito");
+
+        if (newStatus) {
+            await performExternalApiCalls(appointment);
+        }
+    } catch (error) {
+        toast.error(
+            error.response?.data?.message || "Error al actualizar el estatus Develab"
+        );
+        // Reiniciar el estado de éxito en caso de error
+        setSuccessfulUpdates(prev => ({ ...prev, [appointmentId]: false }));
+    }
+};
+
 
     const performExternalApiCalls = async (appointment) => {
         try {
             const loginResponse = await axios.post("https://webapi.devellab.mx/api/Account/login", {
-                username: "mhs",
-                password: "cd098f3b9eae4ae7af3911aec1847d76"
+                username: "preventix",
+                password: "7b5cbac342284010ac18f17ceba6364f"
             });
             const token = loginResponse.data.accessToken;
             setTokenDevel(token);
-
+    
             const patientData = {
                 code: "",
                 name: appointment.patientFirstName,
@@ -106,7 +123,7 @@ const Dashboard = () => {
                 extraField5: "",
                 extraField6: ""
             };
-
+    
             const patientResponse = await axios.post("https://webapi.devellab.mx/api/Patient/", patientData, {
                 headers: {
                     accept: "*/*",
@@ -114,15 +131,16 @@ const Dashboard = () => {
                 }
             });
             const customerId = patientResponse.data.customerId;
-            appointment.ClienteDevelab = customerId;
-
-            const sampleDate = new Date(appointment.fechaToma).toISOString().slice(0, 16);
-
+    
+            // Verificar que 'fechaToma' es un valor de fecha válido
+            const fechaTomaValida = appointment.fechaToma ? new Date(appointment.fechaToma) : new Date();
+            const sampleDate = fechaTomaValida.toISOString().slice(0, 16);
+    
             const orderData = {
-                branchId: 1,
+                branchId: 31,
                 patientId: customerId,
                 observations: "",
-                customerId: appointment.sampleLocationValue,
+                customerId: 1783,
                 customerOrderNumber: "",
                 extraField1: "",
                 extraField2: "",
@@ -137,31 +155,57 @@ const Dashboard = () => {
                 ],
                 sampleDate: sampleDate
             };
-
+    
             const orderResponse = await axios.post("https://webapi.devellab.mx/api/Order/", orderData, {
                 headers: {
                     accept: "*/*",
                     Authorization: `Bearer ${token}`
                 }
             });
-
-            appointment.FolioDevelab = orderResponse.data.orderNumber;
-            appointment.OrderIDDevelab = orderResponse.data.orderId;
-            toast.success("Paciente Cargada exitosamente a Devellab");
+    
+            const updateFields = {
+                FolioDevelab: orderResponse.data.orderNumber,
+                OrderIDDevelab: orderResponse.data.orderId
+            };
+    
+            // Actualizar el appointment en la base de datos local
+            const updateResponse = await axios.put(`https://webapitimser.azurewebsites.net/api/v1/appointment/update/${appointment._id}`, updateFields, {
+                withCredentials: true,
+                headers: { Authorization: `Bearer ${authToken}` }
+            });
+    
+            // Actualizar el estado local para reflejar estos cambios
+            setAppointments((prevAppointments) => prevAppointments.map(appt => {
+                if (appt._id === appointment._id) {
+                    return {
+                        ...appt,
+                        ...updateFields
+                    };
+                }
+                return appt;
+            }));
+    
+            toast.success("Paciente cargado exitosamente a Devellab y actualizado localmente");
         } catch (error) {
-            toast.error("Campos actualizados");
+            toast.error("Error al procesar la información del paciente en Devellab o localmente: " + error.message);
             console.error(error);
         }
     };
+    
 
     const handleDeleteAppointment = async (appointmentId) => {
-        try {
-            await axios.delete(`https://webapitimser.azurewebsites.net/api/v1/appointment/delete/${appointmentId}`, { withCredentials: true, headers: { Authorization: `Bearer ${authToken}` } });
-            setAppointments((prevAppointments) => prevAppointments.filter(appt => appt._id !== appointmentId));
-            toast.success("Cita eliminada con éxito");
-        } catch (error) {
-            toast.error("Error al eliminar la cita");
-            console.error(error);
+        if (window.confirm("¿Estás seguro que deseas eliminar esta cita?")) {
+            try {
+                await axios.delete(`https://webapitimser.azurewebsites.net/api/v1/appointment/delete/${appointmentId}`, {
+                    withCredentials: true,
+                    headers: { Authorization: `Bearer ${authToken}` }
+                });
+                setAppointments((prevAppointments) => prevAppointments.filter(appt => appt._id !== appointmentId));
+                toast.success("Cita eliminada con éxito");
+            } catch (error) {
+                toast.error("Error al eliminar la cita");
+                console.error(error);
+            }
         }
     };
 
@@ -243,6 +287,7 @@ const Dashboard = () => {
                     <thead>
                         <tr>
                             <th>Nombre</th>
+                            <th>Correo</th>
                             <th>Lugar de toma</th>
                             <th>Ayuno</th>
                             <th>Tomada</th>
@@ -263,6 +308,13 @@ const Dashboard = () => {
                                                 type="text"
                                                 name="patientFirstName"
                                                 value={formValues.patientFirstName}
+                                                onChange={handleInputChange}
+                                                className="input"
+                                            />
+                                            <input
+                                                type="text"
+                                                name="email"
+                                                value={formValues.email}
                                                 onChange={handleInputChange}
                                                 className="input"
                                             />
@@ -294,6 +346,7 @@ const Dashboard = () => {
                                 ) : (
                                     <>
                                         <td>{`${appointment.patientFirstName} ${appointment.patientLastName}`}</td>
+                                        <td>{appointment.email}</td>
                                         <td>{appointment.sampleLocation}</td>
                                         <td>{`${appointment.fastingHours} horas`}</td>
                                         <td>
@@ -305,17 +358,18 @@ const Dashboard = () => {
                                             </button>
                                         </td>
                                         <td>
+                                            
+                                            <button
+                                                onClick={() => handleEditClick(appointment)}
+                                                className="update-button1"
+                                            >
+                                                Editar
+                                            </button>
                                             <button
                                                 onClick={() => handleDeleteAppointment(appointment._id)}
                                                 className="delete-button"
                                             >
                                                 Eliminar
-                                            </button>
-                                            <button
-                                                onClick={() => handleEditClick(appointment)}
-                                                className="update-button"
-                                            >
-                                                Editar
                                             </button>
                                         </td>
                                     </>
